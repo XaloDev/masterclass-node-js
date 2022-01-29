6b5d32b3ea29f3312cdc825516f82f6ff5a72dfd
Xalo Dev <xalodeveloper@gmail.com>
2022-01-29 18:15:44 -0300

    and stats

---
+++ stats.js
@@ -0,0 +1,24 @@
+const os = require('os')
+const { freemem, totalmem } = os
+
+setInterval(() => {
+    const using = parseInt(totalmem() / 1024 / 1024) - parseInt(freemem() / 1024 / 1024)
+    const free = parseInt(freemem() / 1024 / 1024)
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const percents = parseInt(using / total * 100)
+    
+    const stats = {  
+        free: `${free} MB`,
+        using: `${using} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    }
+    
+    console.clear()
+    console.log("=== PC STATS ===")
+    console.table(stats)
+    
+}, 1000)
+
+
+
