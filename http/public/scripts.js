57e8ac6d5e25a1b89aef3ae256bd60a2421ec4f5
Xalo Dev <xalodeveloper@gmail.com>
2022-01-30 13:49:55 -0300

    final commit

--- http/public/scripts.js
+++ http/public/scripts.js
@@ -3,7 +3,15 @@ const input = document.querySelector("input")
 const form = document.querySelector('form')
 
 
-function addElement({ name, url }) {
+async function load(){
+    const res = await fetch("http://localhost:3000/").then(data => data.json())
+    
+    res.urls.forEach(element => addElement({...element, firstLoad: true}))
+}
+
+load()
+
+function addElement({ name, url, firstLoad }) {
     const li = document.createElement('li')
     const a = document.createElement("a")
     const trash = document.createElement("span")
@@ -12,16 +20,27 @@ function addElement({ name, url }) {
     a.innerHTML = name
     a.target = "_blank"
 
+    const baseUrl = "http://localhost:3000/" + `?name=${name}&url=${url}`
+
+    if(!firstLoad){
+        fetch(baseUrl)
+    }
+
+    const deleteUrl = "http://localhost:3000/" + `?name=${name}&url=${url}&del=1`
+
     trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+    trash.onclick = () => removeElement(trash, deleteUrl)
 
     li.append(a)
     li.append(trash)
     ul.append(li)
+
+    
 }
 
-function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
+function removeElement(el, deleteUrl) {
+    if (confirm('Are you sure you want to delete?'))
+        fetch(deleteUrl)
         el.parentNode.remove()
 }
 
@@ -31,15 +50,15 @@ form.addEventListener("submit", (event) => {
     let { value } = input
 
     if (!value) 
-        return alert('Preencha o campo')
+        return alert('Fill the field')
 
     const [name, url] = value.split(",")
 
     if (!url) 
-        return alert('formate o texto da maneira correta')
+        return alert('Format the text correctly')
 
     if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta")
+        return alert("Type the url correctly")
 
     addElement({ name, url })
 
