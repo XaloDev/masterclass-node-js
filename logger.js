fe45448a498d2491f53ef6ec9ce5fa0c40baf975
Xalo Dev <xalodeveloper@gmail.com>
2022-01-30 12:29:10 -0300

    and logger

---
+++ logger.js
@@ -0,0 +1,17 @@
+const EventEmmiter = require('events')
+const fs = require('fs')
+const path = require('path')
+
+const emitter = new EventEmmiter()
+
+emitter.on('log', message => {
+    fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
+        if (err) throw err
+    })
+})
+
+function log(message){
+    emitter.emit('log', message)
+}
+
+module.exports = log
--- stats.js
+++ stats.js
@@ -1,5 +1,6 @@
 const os = require('os')
 const { freemem, totalmem } = os
+const log = require('./logger')
 
 setInterval(() => {
     const using = parseInt(totalmem() / 1024 / 1024) - parseInt(freemem() / 1024 / 1024)
@@ -17,7 +18,9 @@ setInterval(() => {
     console.clear()
     console.log("=== PC STATS ===")
     console.table(stats)
-    
+
+    log(`${JSON.stringify(stats)}\n`)
+
 }, 1000)
 
 
